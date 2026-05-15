515df930f1a4e15b2043b9c646fa66cf87623693
Van Taylor <Van.Taylor+church@churchofjesuschrist.org>
2026-05-15 17:34:44 -0600

    finally done with the solo team project.

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -1,40 +1,78 @@
-﻿/// <summary>
+﻿using Microsoft.VisualBasic;
+
+/// <summary>
 /// Maintain a Customer Service Queue.  Allows new customers to be 
 /// added and allows customers to be serviced.
 /// </summary>
-public class CustomerService {
-    public static void Run() {
+public class CustomerService
+{
+    public static void Run()
+    {
         // Example code to see what's in the customer service queue:
         // var cs = new CustomerService(10);
         // Console.WriteLine(cs);
 
         // Test Cases
-
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Add one customer serve one cusomer.
+        // Expected Result: Customer gets added then removed from the queue with a size of 10. (queue size un-applicable)
         Console.WriteLine("Test 1");
+        var service = new CustomerService(10);
+        service.AddNewCustomer();
+        service.ServeCustomer();
+        // ResultL: Index was out of range
 
-        // Defect(s) Found: 
+
+
+        // Defect(s) Found: we were removing the customer before assigning it to a variable which lead to removing where the customer used to be not where it was in the queue. 
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Add two customers, then serve two customers
+        // Expected Result: They get served in the correct order.
         Console.WriteLine("Test 2");
+        service = new CustomerService(5);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.ServeCustomer();
+        service.ServeCustomer();
 
         // Defect(s) Found: 
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+
+        // Test 3
+        // Scenario: Add six customers to a queue that should only hold five.
+        // Expected Result: I get an error saying the Queue is full. 
+        Console.WriteLine("Test 3");
+        service = new CustomerService(5);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+
+
+
+        // Defect(s) Found: The queue was set to check if the queue was bigger than the max size when it should have been checking if they were equal. 
+
+        Console.WriteLine("=================");
+
+        // Add more Test Cases As Needed Below
+
+
+
     }
 
     private readonly List<Customer> _queue = new();
     private readonly int _maxSize;
 
-    public CustomerService(int maxSize) {
+    public CustomerService(int maxSize)
+    {
         if (maxSize <= 0)
             _maxSize = 10;
         else
@@ -45,8 +83,10 @@ public class CustomerService {
     /// Defines a Customer record for the service queue.
     /// This is an inner class.  Its real name is CustomerService.Customer
     /// </summary>
-    private class Customer {
-        public Customer(string name, string accountId, string problem) {
+    private class Customer
+    {
+        public Customer(string name, string accountId, string problem)
+        {
             Name = name;
             AccountId = accountId;
             Problem = problem;
@@ -56,7 +96,8 @@ public class CustomerService {
         private string AccountId { get; }
         private string Problem { get; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return $"{Name} ({AccountId})  : {Problem}";
         }
     }
@@ -65,9 +106,11 @@ public class CustomerService {
     /// Prompt the user for the customer and problem information.  Put the 
     /// new record into the queue.
     /// </summary>
-    private void AddNewCustomer() {
+    private void AddNewCustomer()
+    {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count == _maxSize)
+        {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -87,9 +130,11 @@ public class CustomerService {
     /// <summary>
     /// Dequeue the next customer and display the information.
     /// </summary>
-    private void ServeCustomer() {
+    private void ServeCustomer()
+    {
+        Console.WriteLine(_queue.Count());
+        var customer = _queue[0]; // this was happening after _queue.RemoveAt(0) which meant it was trying to remove a customer before saving it, so I would end up grabbing where the customer used to be but because there was only one customer, there was nothing there to grab. 
         _queue.RemoveAt(0);
-        var customer = _queue[0];
         Console.WriteLine(customer);
     }
 
@@ -100,7 +145,8 @@ public class CustomerService {
     /// see the contents.
     /// </summary>
     /// <returns>A string representation of the queue</returns>
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"[size={_queue.Count} max_size={_maxSize} => " + string.Join(", ", _queue) + "]";
     }
 }
